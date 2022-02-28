3dab19e53271711fe213ff1a0c39b74fc49b4da5
gabryelsoares <gabryelfhsoares@gmail.com>
2022-02-28 14:55:03 -0300

    update Loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,12 +1,15 @@
 import { LoaderSpin } from '../LoaderSpin';
 import { Container } from './styled';
 
+type Props = {
+  text?: string;
+}
 
-export const Loading = () => {
+export const Loading = ({ text = 'Carregando...' }: Props) => {
   return (
     <Container>
       <LoaderSpin size={120} borderSize={16} />
-      Carregando...
+      {text}
     </Container>
   );
 
